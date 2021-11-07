a548e9d04bb0962cf84e12b704837aba4878c029
Federico Cecchetto <fedecek3@gmail.com>
2021-11-07 02:06:03 +0100

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -5,8 +5,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH)
     {
-        const auto value = *reinterpret_cast<DWORD*>(0x20700000);
-        if (value != 0x2A6784ED)
+        const auto value = *reinterpret_cast<DWORD*>(0x20800000);
+        if (value != 0xB9C9566)
         {
             printf("\x1b[31m\n**************************************************************************************\n\n");
             printf("This version of \x1b[33miw5-gsc-utils\x1b[31m is outdated.\n");
