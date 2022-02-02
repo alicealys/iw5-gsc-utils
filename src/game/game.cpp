ccd8176c2573d10719ba66789c31169b9afdaa74
Diavolo <edoardo.sanguineti222@gmail.com>
2022-02-02 15:24:20 +0100

    Remove assert

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -4,8 +4,6 @@ namespace game
 {
 	void SV_GameDropClient(int clientNum, const char* reason)
 	{
-		assert(sv_maxclients->current.integer >= 1 && sv_maxclients->current.integer <= 18);
-
 		if (clientNum >= 0 && clientNum < sv_maxclients->current.integer)
 		{
 			SV_DropClient(&svs_clients[clientNum], reason, true);
