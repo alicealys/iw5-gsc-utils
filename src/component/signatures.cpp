1d94fdd88d463f20163f69e1f5beb0e0fabe7038
fed <58637860+fedddddd@users.noreply.github.com>
2022-12-28 01:07:53 +0100

    Small fix

--- src/component/signatures.cpp
+++ src/component/signatures.cpp
@@ -53,7 +53,7 @@ namespace signatures
 		char bytes[4] = {0};
 		const auto string_ptr = find_string_ptr(string);
 		memcpy(bytes, &string_ptr, sizeof(bytes));
-		return find_string_ptr(bytes);
+		return find_string_ptr({bytes, 4});
 	}
 
 	bool process_maps()
